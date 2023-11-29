23a10d432cae134aea4601e87d24cbef6753ae2b
Odlanyer Liaquen <143677217+Derthings@users.noreply.github.com>
2023-11-29 17:46:26 +0800

    update

--- WST Final Project/confirm_order.php
+++ WST Final Project/confirm_order.php
@@ -17,22 +17,36 @@ if ($conn->connect_error) {
 $customerName = $_POST['customer-name'];
 $orderDate = $_POST['order-date'];
 
-// Insert data into the database
-$sql = "INSERT INTO tbl_customers (customer_name, order_date) VALUES ('$customerName', '$orderDate')";
+// Insert data into the tbl_customers
+$sqlInsertCustomer = "INSERT INTO tbl_customers (customer_name, order_date) VALUES ('$customerName', '$orderDate')";
 
-if ($conn->query($sql) === TRUE) {
-    // If the insertion is successful, return the order number
-    $orderNumber = generateOrderNumber();
-    echo json_encode(array('status' => 'success', 'orderNumber' => $orderNumber));
+if ($conn->query($sqlInsertCustomer) === TRUE) {
+    // If the insertion is successful, retrieve the customer ID
+    $customerId = $conn->insert_id;
+
+    // Generate a unique order number using customer ID and a random number
+    $orderNumber = generateOrderNumber($customerId);
+
+    // Update the order number in tbl_customers
+    $sqlUpdateOrderNumber = "UPDATE tbl_customers SET order_number = '$orderNumber' WHERE customer_id = $customerId";
+    
+    if ($conn->query($sqlUpdateOrderNumber) === TRUE) {
+        // Return the response with customer ID and order number
+        echo json_encode(array('status' => 'success', 'customerId' => $customerId, 'orderNumber' => $orderNumber));
+    } else {
+        // If there's an error in updating order number, return an error status
+        echo json_encode(array('status' => 'error', 'message' => $conn->error));
+    }
 } else {
-    // If there's an error, return an error status
+    // If there's an error in inserting customer information, return an error status
     echo json_encode(array('status' => 'error', 'message' => $conn->error));
 }
 
 // Close the database connection
 $conn->close();
 
-function generateOrderNumber() {
-    return rand(100000, 999999); // Adjust as needed
+function generateOrderNumber($customerId) {
+    // Combine customer ID with a random number
+    return $customerId . rand(1000, 9999); // Adjust as needed
 }
 ?>
