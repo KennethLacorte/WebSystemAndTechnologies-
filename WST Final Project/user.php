3ed0192f0ab41061cb4fc7a5a7c53a2eaa5c0d64
John Kenneth Lacorte <21-34253@g.batstate-u.edu.ph>
2023-12-03 22:48:20 +0800

    fixed where product price updating

--- WST Final Project/user.php
+++ WST Final Project/user.php
@@ -199,98 +199,78 @@ $result = $conn->query($sql);
         </form>
 
         <script>
-  document.addEventListener('DOMContentLoaded', function () {
-    const addToOrderButtons = document.querySelectorAll('.add-to-order-button');
-    const orderItemsContainer = document.getElementById('order-items');
-    const customerInfoForm = document.getElementById('confirm-order-form');
-    const overallTotalElement = document.getElementById('overall-total');
+            document.addEventListener('DOMContentLoaded', function () {
+                const addToOrderButtons = document.querySelectorAll('.add-to-order-button');
+                const orderItemsContainer = document.getElementById('order-items');
+                const customerInfoForm = document.getElementById('confirm-order-form');
+                const overallTotalElement = document.getElementById('overall-total');
 
-    // Hide the customer information form initially
-    customerInfoForm.style.display = 'none';
-
-    // Variable to store the overall total
-    let overallTotal = 0.00;
-
-    addToOrderButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            // Check if the button is not disabled
-            if (!button.disabled) {
-                const productName = button.dataset.productName;
-                const productPrice = parseFloat(button.dataset.productPrice); // Parse as float
-
-                // Call a function to add the item to the order form and update the overall total
-                addToOrder(productName, productPrice);
-
-                // Disable the button to prevent further clicks
-                button.disabled = true;
-
-                // Show customer information fields and submit button
-                showCustomerInfoForm();
-            }
-        });
-    });
-
-    // Function to add items to the order form
-    function addToOrder(productName, productPrice) {
-        const row = document.createElement('tr');
-        row.innerHTML = `<td>${productName}</td><td>${productPrice.toFixed(2)}</td><td><input type="number" name="quantity[]" value="1" min="1"></td><td><button class="remove-item-button">Remove</button></td>`;
-        orderItemsContainer.appendChild(row);
-
-        // Calculate the total for the current item and update the overall total
-        const quantityInput = row.querySelector('input[name="quantity[]"]');
-        quantityInput.addEventListener('input', function () {
-            updateItemTotal(row, productPrice);
-        });
-
-        updateItemTotal(row, productPrice);
-
-        // Add event listener to the newly added remove button
-        const removeButton = row.querySelector('.remove-item-button');
-        removeButton.addEventListener('click', function () {
-            // Remove the item from the order
-            row.remove();
-
-            // Enable the corresponding "Add to Order" button
-            const correspondingAddButton = Array.from(addToOrderButtons).find(btn => btn.dataset.productName === productName);
-            if (correspondingAddButton) {
-                correspondingAddButton.disabled = false;
-            }
-
-            // Check if there are no items in the order, then hide the customer information form
-            if (orderItemsContainer.children.length === 0) {
                 customerInfoForm.style.display = 'none';
-            }
+                let overallTotal = 0.00;
 
-            // Update the overall total after removing an item
-            overallTotal -= parseFloat(row.dataset.itemTotal);
-            overallTotalElement.textContent = overallTotal.toFixed(2);
-        });
-    }
+                addToOrderButtons.forEach(button => {
+                    button.addEventListener('click', function () {
+                        if (!button.disabled) {
+                            const productName = button.dataset.productName;
+                            const productPrice = parseFloat(button.dataset.productPrice);
 
-    // Function to update the item total and overall total
-    function updateItemTotal(row, productPrice) {
-        const quantityInput = row.querySelector('input[name="quantity[]"]');
-        const quantity = parseInt(quantityInput.value, 10);
-        const itemTotal = productPrice * quantity;
+                            addToOrder(productName, productPrice);
 
-        // Update the data attribute with the item total
-        row.dataset.itemTotal = itemTotal;
+                            button.disabled = true;
+                            showCustomerInfoForm();
+                        }
+                    });
+                });
 
-        // Update the total for the current item
-        row.querySelector('td:nth-child(2)').textContent = itemTotal.toFixed(2);
+                function addToOrder(productName, productPrice) {
+                    const row = document.createElement('tr');
+                    const originalPrice = productPrice;
 
-        // Update the overall total
-        overallTotal = Array.from(orderItemsContainer.children).reduce((total, item) => total + parseFloat(item.dataset.itemTotal), 0);
-        overallTotalElement.textContent = overallTotal.toFixed(2);
-    }
+                    row.innerHTML = `<td>${productName}</td><td>${originalPrice.toFixed(2)}</td><td><input type="number" name="quantity[]" value="1" min="1"></td><td><button class="remove-item-button">Remove</button></td>`;
+                    orderItemsContainer.appendChild(row);
 
-    // Function to show customer information fields and submit button
-    function showCustomerInfoForm() {
-        customerInfoForm.style.display = 'block';
-    }
-});
+                    const quantityInput = row.querySelector('input[name="quantity[]"]');
+                    quantityInput.addEventListener('input', function () {
+                        updateItemTotal(row, originalPrice);
+                    });
 
-</script>
+                    updateItemTotal(row, originalPrice);
+
+                    const removeButton = row.querySelector('.remove-item-button');
+                    removeButton.addEventListener('click', function () {
+                        row.remove();
+
+                        const correspondingAddButton = Array.from(addToOrderButtons).find(btn => btn.dataset.productName === productName);
+                        if (correspondingAddButton) {
+                            correspondingAddButton.disabled = false;
+                        }
+
+                        if (orderItemsContainer.children.length === 0) {
+                            customerInfoForm.style.display = 'none';
+                        }
+
+                        overallTotal -= parseFloat(row.dataset.itemTotal);
+                        overallTotalElement.textContent = overallTotal.toFixed(2);
+                    });
+                }
+
+                function updateItemTotal(row, originalPrice) {
+                    const quantityInput = row.querySelector('input[name="quantity[]"]');
+                    const quantity = parseInt(quantityInput.value, 10);
+                    const itemTotal = originalPrice * quantity;
+
+                    row.dataset.itemTotal = itemTotal;
+                    row.querySelector('td:nth-child(2)').textContent = originalPrice.toFixed(2);
+
+                    overallTotal = Array.from(orderItemsContainer.children).reduce((total, item) => total + parseFloat(item.dataset.itemTotal), 0);
+                    overallTotalElement.textContent = overallTotal.toFixed(2);
+                }
+
+                function showCustomerInfoForm() {
+                    customerInfoForm.style.display = 'block';
+                }
+            });
+        </script>
 
 <script src="add-order.js"></script>
 <script src="search.js"></script>
@@ -411,7 +391,7 @@ $result = $conn->query($sql);
                         }
                     }
                 }
-                x
+     
 
                 generateOrderNumber() {
                     return Math.floor(Math.random() * 1000000) + 1;
