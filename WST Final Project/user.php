c1a92f1d9448f59aa33d1a9acd9fc559ddb9f69f
Odlanyer Liaquen <143677217+Derthings@users.noreply.github.com>
2023-12-03 16:35:03 +0800

    updated navbar

--- WST Final Project/user.php
+++ WST Final Project/user.php
@@ -45,69 +45,92 @@ $result = $conn->query($sql);
 
 <body>
     <header>
-        <div class="navbar">
-            <div class="logo"><a href="#">Angel's Burger</a></div>
-            <ul class="links">
-                <li><a href="#" onclick="loadPage('home-content')" style="color: white;">HOME</a></li>
-                <li><a href="#" onclick="loadPage('addorder-content')" style="color: white;">ADD ORDER</a></li>
-                <li><a href="#" onclick="loadPage('order-content')" style="color: white;">ORDERS</a></li>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h3>Angels Burger</h3></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-pills mb-3" id="pills-tab" role="tablist"">
+        <li class="nav-item ps-3 role="presentation">
+          <a  class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" href="#" onclick="loadPage('home-content')" style="color: white;">HOME</a>
+        </li>
+        <li class="nav-item ps-3">   
+          <a  class="nav-link " id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" href="#" onclick="loadPage('addorder-content')" style="color: white;">ADD ORDER</a></li>
+        </li>
+        <li class="nav-item ps-3">
+          <a  class="nav-link " id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" href="#" onclick="loadPage('order-content')" style="color: white;">ORDERS</a>
+        </li>
+        
+        <li class="dropdown ps-3" data-bs-theme="dark">
+          <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              MENU
+          </button>
+          <ul class="dropdown-menu">
+            <li class="menu-items"><a href="#" data-content-id="hamburgers-content" >ABOUT US</a></li>
+            <li class="menu-items"><a href="#" data-content-id="hamburgers-content" >HAMBURGERS</a></li>
+            <li class="menu-items"><a href="#" data-content-id="hotdogs-content" >HOTDOG SANDWICHES</a></li>
+            <li class="menu-items"><a href="#" data-content-id="hamsandwiches-content" >HAM SANDWICHES</a></li>
+            <li class="menu-items"><a href="#" data-content-id="baconsandwiches-content" >BACON SANDWICHES</a></li>
+            <li class="menu-items"><a href="#" data-content-id="drinks-content" >DRINKS</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex "  role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
-                <li class="dropdown" data-bs-theme="dark">
-                    <button class="btn btn-info btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        MENU
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li class="menu-items"><a href="#" data-content-id="hamburgers-content" style="color: white;">ABOUT US</a></li>
-                        <li class="menu-items"><a href="#" data-content-id="hamburgers-content" style="color: white;">HAMBURGERS</a></li>
-                        <li class="menu-items"><a href="#" data-content-id="hotdogs-content" style="color: white;">HOTDOG SANDWICHES</a></li>
-                        <li class="menu-items"><a href="#" data-content-id="hamsandwiches-content" style="color: white;">HAM SANDWICHES</a></li>
-                        <li class="menu-items"><a href="#" data-content-id="baconsandwiches-content" style="color: white;">BACON SANDWICHES</a></li>
-                        <li class="menu-items"><a href="#" data-content-id="drinks-content" style="color: white;">DRINKS</a></li>
-
-                    </ul>
-                </li>
-            </ul>
-
-
-
-
-            <div class="search-bar">
-                <input type="text" placeholder="Search...">
-                <button type="button"><i class="fas fa-search"></i></button>
-            </div>
-        </div>
     </header>
 
     <section class="content-section" id="home-content">
         <h1><strong>ANGELS BURGER!</strong></h1>
 
 
-        <div id="carouselExampleRide" class="carousel slide d-flex align-items-center" data-bs-ride="carousel" data-bs-interval="2000" style="max-width: 60%; height: 70vh; left: 300px;">
-            <div class="carousel-inner col-3">
-                <div class="carousel-item active">
-                    <img src="hamburger1.jpg" class="d-block w-75 mx-auto" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="hamburger1.jpg" class="d-block w-75 mx-auto" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="angelsbg.jpg" class="d-block w-75 mx-auto" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black;"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black;"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-
-
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="angelsbg.jpg" class="d-block" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="angelsbg.jpg" class="d-block" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="angelsbg.jpg" class="d-block" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
     </section>
-
 
 
     <script src="crud.js"></script>
@@ -172,7 +195,6 @@ $result = $conn->query($sql);
             }
             ?>
         </form>
-
         <script>
             document.addEventListener('DOMContentLoaded', function() {
                 const addToOrderButtons = document.querySelectorAll('.add-to-order-button');
