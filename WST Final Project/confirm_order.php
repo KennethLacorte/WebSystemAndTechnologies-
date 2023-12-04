ce1e58994b1bb9571aebe51d0dceec129221e81b
Odlanyer Liaquen <143677217+Derthings@users.noreply.github.com>
2023-12-04 13:27:51 +0800

    updated 1

--- WST Final Project/confirm_order.php
+++ WST Final Project/confirm_order.php
@@ -24,20 +24,40 @@ if ($conn) {
     if ($conn->query($sqlCustomer) === TRUE) {
         $customerId = $conn->insert_id;
 
-    // Insert data into tbl_orders
-    $orderNumber = $data['orderNumber'];
-    $totalPrice = $data['totalPrice'];  // Fix the key name to totalPrice
+        // Insert data into tbl_orders
+        $orderNumber = $data['orderNumber'];
+        $totalPrice = $data['totalPrice'];
 
-    $sqlOrder = "INSERT INTO tbl_orders (order_number, customer_id, totals) VALUES ('$orderNumber', '$customerId', '$totalPrice')";
-    if ($conn->query($sqlOrder) === TRUE) {
-        $response = ["status" => "success", "message" => "Order confirmed and data inserted successfully"];
-        echo json_encode($response);
+        $sqlOrder = "INSERT INTO tbl_orders (order_number, customer_id, totals) VALUES ('$orderNumber', '$customerId', '$totalPrice')";
+        if ($conn->query($sqlOrder) === TRUE) {
+            $orderId = $conn->insert_id;
+
+            // Insert order items data
+            foreach ($data['orderItems'] as $item) {
+                $itemID = $item['itemID'];
+                $quantity = $item['quantity'];
+
+                // Assuming tbl_items has item_id column
+                $sqlOrderItems = "INSERT INTO tbl_order_items (order_id, item_id, quantity) VALUES ('$orderId', '$itemID', '$quantity')";
+
+                if (!$conn->query($sqlOrderItems)) {
+                    $response = ["status" => "error", "message" => "Error inserting order items data: " . $conn->error];
+                    echo json_encode($response);
+                    exit;
+                }
+            }
+
+            // Handle the response after the loop
+            $response = ["status" => "success", "message" => "Order confirmed and data inserted successfully"];
+        } else {
+            $response = ["status" => "error", "message" => "Error inserting order data: " . $conn->error];
+        }
     } else {
-        $response = ["status" => "error", "message" => "Error inserting order data: " . $conn->error];
-        echo json_encode($response);
+        $response = ["status" => "error", "message" => "Error inserting customer data: " . $conn->error];
     }
-} else {
-    $response = ["status" => "error", "message" => "Error inserting customer data: " . $conn->error];
+
+    closeConnection($conn);
+
     echo json_encode($response);
 } else {
     echo json_encode(["status" => "error", "message" => "Database connection error"]);
--- WST Final Project/user.php
+++ WST Final Project/user.php
@@ -281,7 +281,6 @@ $result = $conn->query($sql);
         <script src="search.js"></script>
     </section>
 
-
     <section class="content-section" id="order-content">
         <h1>ORDER PAGE</h1>
         <p>This is the content for the ORDER page...</p>
@@ -320,145 +319,119 @@ $result = $conn->query($sql);
 
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-        <!-- ... existing code ... -->
-
-        <!-- ... existing code ... -->
-
         <script>
-            class OrderConfirmation {
-                constructor() {
-                    // Set the current date when an instance is created
-                    this.setCurrentDate();
-                }
+    class OrderConfirmation {
+        constructor() {
+            this.setCurrentDate();
+        }
 
-                setCurrentDate() {
-                    var currentDate = new Date().toISOString().split('T')[0];
-                    this.orderDate = currentDate;
-                    document.getElementById('order-date').value = currentDate;
-                }
+        setCurrentDate() {
+            var currentDate = new Date().toISOString().split('T')[0];
+            this.orderDate = currentDate;
+            document.getElementById('order-date').value = currentDate;
+        }
 
-                async confirmOrder() {
-                    // Get customer name from the form
-                    var customerName = document.getElementById('customer-name').value;
+        async confirmOrder() {
+            var customerName = document.getElementById('customer-name').value;
 
-                    // Validate customer name (you may add more validation as needed)
-                    if (customerName.trim() === '') {
-                        // Display SweetAlert for validation error
-                        this.showValidationError('Please enter customer name.');
-                        return;
-                    }
-
-                    // Generate an auto-generated order number
-                    var orderNumber = this.generateOrderNumber();
-
-                    // Display SweetAlert with customer details and order confirmation
-                    const result = await Swal.fire({
-                        title: 'Order Confirmation',
-                        html: `
-            <p><strong>Customer Name:</strong> ${customerName}</p>
-            <p><strong>Order Date:</strong> ${this.orderDate}</p>
-            <p><strong>Order Number:</strong> ${orderNumber}</p>
-            <p><strong>Total Price:</strong> $${this.calculateTotalPrice()}</p>
-            <p>Order confirmed! Thank you for your purchase.</p>
-        `,
-                        icon: 'success',
-                        confirmButtonText: 'OK'
-                    });
-
-                    if (result.isConfirmed) {
-                        // Reset the form after confirmation
-                        document.getElementById('confirm-order-form').reset();
-
-                        // Perform additional actions here, such as making an AJAX request to confirm the order on the server
-                        // Example:
-                        const response = await fetch('confirm_order.php', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                                customerName,
-                                orderDate: this.orderDate,
-                                orderNumber,
-                                totalPrice: this.calculateTotalPrice(),
-                                // Add other data if needed
-                            }),
-                        });
-
-                        // Handle the response as needed
-                        if (response.ok) {
-                            const data = await response.json();
-                            console.log('Server response:', data);
-                            // You can add more handling based on the server response
-                        } else {
-                            console.error('Error confirming order on the server');
-                            // You can display an error message to the user if needed
-                        }
-                    }
-                }
-                x
-
-                generateOrderNumber() {
-                    return Math.floor(Math.random() * 1000000) + 1;
-                }
-
-                calculateTotalPrice() {
-                    let total = 0;
-                    const orderItems = document.querySelectorAll('#order-items tr');
-
-                    orderItems.forEach((item) => {
-                        const price = parseFloat(item.querySelector('td:nth-child(2)').innerText);
-                        const quantity = parseInt(item.querySelector('td:nth-child(3) input').value, 10);
-                        total += price * quantity;
-                    });
-
-                    return total.toFixed(2);
-                }
-
-
-                showValidationError(message) {
-                    Swal.fire({
-                        title: 'Validation Error',
-                        text: message,
-                        icon: 'error',
-                        confirmButtonText: 'OK'
-                    });
-                }
-
-                updateItemTotal(row, productPrice) {
-                    const quantityInput = row.querySelector('input[name="quantity[]"]');
-                    const quantity = parseInt(quantityInput.value, 10);
-                    const itemTotal = productPrice * quantity;
-
-                    // Update the data attribute with the item total
-                    row.dataset.itemTotal = itemTotal;
-
-                    // Update the total for the current item
-                    row.querySelector('td:nth-child(2)').textContent = itemTotal.toFixed(2);
-
-                    // Update the overall total
-                    overallTotal = Array.from(orderItemsContainer.children).reduce((total, item) => total + parseFloat(item.dataset.itemTotal), 0);
-                    overallTotalElement.textContent = overallTotal.toFixed(2);
-                }
-
-                showCustomerInfoForm() {
-                    // Assuming you have a customerInfoForm variable, you can toggle its display
-                    const customerInfoForm = document.getElementById('customer-info-form');
-                    customerInfoForm.style.display = 'block';
-                }
+            if (customerName.trim() === '') {
+                this.showValidationError('Please enter customer name.');
+                return;
             }
 
-            // Create an instance of the OrderConfirmation class
-            const orderConfirmation = new OrderConfirmation();
+            var orderNumber = this.generateOrderNumber();
+            var totalPrice = this.calculateTotalPrice();
 
-            // Attach the confirmOrder method to the button click event
-            document.getElementById('submit-btn').addEventListener('click', () => orderConfirmation.confirmOrder());
-        </script>
+            const result = await Swal.fire({
+                title: 'Order Confirmation',
+                html: `
+                    <p><strong>Customer Name:</strong> ${customerName}</p>
+                    <p><strong>Order Date:</strong> ${this.orderDate}</p>
+                    <p><strong>Order Number:</strong> ${orderNumber}</p>
+                    <p><strong>Total Price:</strong> $${totalPrice}</p>
+                    <p>Order confirmed! Thank you for your purchase.</p>
+                `,
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+
+            if (result.isConfirmed) {
+                document.getElementById('confirm-order-form').reset();
+
+                const response = await fetch('confirm_order.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        customerName,
+                        orderDate: this.orderDate,
+                        orderNumber,
+                        totalPrice,
+                        orderItems: this.getOrderItemsData(), // Add order items data
+                    }),
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log('Server response:', data);
+                } else {
+                    console.error('Error confirming order on the server');
+                }
+            }
+        }
+
+        generateOrderNumber() {
+            return Math.floor(Math.random() * 1000000) + 1;
+        }
+
+        calculateTotalPrice() {
+            let total = 0;
+            const orderItems = document.querySelectorAll('#order-items tr');
+
+            orderItems.forEach((item) => {
+                const price = parseFloat(item.querySelector('td:nth-child(2)').innerText);
+                const quantity = parseInt(item.querySelector('td:nth-child(3) input').value, 10);
+                total += price * quantity;
+            });
+
+            return total.toFixed(2);
+        }
+
+        showValidationError(message) {
+            Swal.fire({
+                title: 'Validation Error',
+                text: message,
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        }
+
+        getOrderItemsData() {
+            const orderItems = document.querySelectorAll('#order-items tr');
+            const orderItemsData = [];
+
+            orderItems.forEach((item) => {
+                const itemData = {
+                    productID: item.dataset.productId, // Add the product ID here
+                    itemID: item.dataset.itemId, // Add the item ID here
+                    quantity: parseInt(item.querySelector('td:nth-child(3) input').value, 10),
+                };
+
+                orderItemsData.push(itemData);
+            });
+
+            return orderItemsData;
+        }
+    }
+
+    const orderConfirmation = new OrderConfirmation();
+    document.getElementById('submit-btn').addEventListener('click', () => orderConfirmation.confirmOrder());
+</script>
+
 
 
     </section>
-
-
 
 
 
